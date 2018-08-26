a5363b8f8123ac9f801531c7ede675b66b9e23ea
AzureAD\CasimiroRocha <spideykk@gmail.com>
2018-08-26 17:56:52 -0300

    Animación finalizada

---
+++ Proyecto2D/Assets/Scripts/Efectos.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Efectos : MonoBehaviour {
+
+	public void playEffect()
+    {
+        GetComponent<AudioSource>().Play();
+    }
+}
